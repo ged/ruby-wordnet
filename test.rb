d5c4897d121d6ddf2b93e63661e7bf684012ab3e
Michael Granger <ged@FaerieMUD.org>
2002-01-14 13:34:06 +0000

    - Converted to a RubyUnit test, and walkit is no longer maintained or available.

--- test.rb
+++ test.rb
@@ -4,28 +4,44 @@
 #
 #
 
-require "walkit/cli_script"
+require "runit/testsuite"
+require "runit/cui/testrunner"
+require "parsearg"
 require "find"
+
+# RUNIT::CUI::TestRunner.quiet_mode = true
 
 $: << "lib"
 
-### Load all the tests from the tests dir
-puts "Finding test files..."
-Find.find("t") {|file|
-	Find.prune if file =~ %r{^\.\.|.*/\.}
-	next if File.stat( file ).directory?
-	next unless file =~ /.*_tests\.rb$/
-	puts "  Requiring #{file}..."
-	require( file )
-}
-
-### Find all the loaded test classes
-testClasses = []
-ObjectSpace.each_object( Class ) {|klass|
-	next unless klass < Walkit::Testclass
-	testClasses << klass
-}
-
 ### Run 'em, or run the ones that match Regexp.new(ARGV[0]), if specified.
-Walkit::Cli_script.new.select( testClasses.sort, $*.shift )
+class TestAll
+	def TestAll.suite
+
+		# Load all the tests from the tests dir
+		puts "Finding test files..."
+		Find.find("t") {|file|
+			Find.prune if file =~ %r{^\.\.|.*/\.}
+			next if File.stat( file ).directory?
+			next unless file =~ /.*\.rb$/
+			puts "  Requiring #{file}..."
+			require( file )
+		}
+		# Find all the loaded test classes
+		testClasses = []
+		ObjectSpace.each_object( Class ) {|klass|
+			next unless klass < RUNIT::TestCase
+			testClasses << klass
+		}
+
+		# Define a test suite made up of all the RUNIT::TestCase suites
+		suite = RUNIT::TestSuite.new
+		testClasses.sort.each {|klass|
+			suite.add(klass.suite)
+		}
+		return suite
+	end
+end
+
+RUNIT::CUI::TestRunner.run( TestAll.suite )
+
 
