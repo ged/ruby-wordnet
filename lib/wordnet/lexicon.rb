59b1ab6b6bdaa47da2595d4d9ea064a482a68ec2
Michael Granger <ged@FaerieMUD.org>
2003-08-06 08:07:04 +0000

    - Modified to use BDB transactions.
    - Made changes to reflect change in constant names.

--- lib/wordnet/lexicon.rb
+++ lib/wordnet/lexicon.rb
@@ -1,20 +1,21 @@
+#!/usr/bin/ruby
 #
-# WordNet lexicon object class
+# WordNet Lexicon object class
 # 
-# = Synopsis
+# == Synopsis
 # 
 #   lexicon = WordNet::Lexicon.new( dictpath )
 # 
-# = Description
+# == Description
 # 
 # Instances of this class abstract access to the various databases of the
-# WordNet lexicon. It can be used to look up and search for +WordNet::Synset+s.
+# WordNet lexicon. It can be used to look up and search for WordNet::Synsets.
 # 
-# = Author
+# == Author
 # 
 # Michael Granger <ged@FaerieMUD.org>
 # 
-# Copyright (c) 2002 The FaerieMUD Consortium. All rights reserved.
+# Copyright (c) 2002, 2003 The FaerieMUD Consortium. All rights reserved.
 # 
 # This module is free software. You may use, modify, and/or redistribute this
 # software under the terms of the Perl Artistic License. (See
@@ -23,289 +24,281 @@
 # Much of this code was inspired by/ported from the Lingua::Wordnet Perl module
 # by Dan Brian.
 # 
-# = Version
+# == Version
 #
-# $Id: lexicon.rb,v 1.2 2002/01/14 13:41:27 deveiant Exp $
+# $Id: lexicon.rb,v 1.3 2003/08/06 08:07:04 deveiant Exp $
 # 
 
-require "bdb"
-require "sync"
+require 'bdb'
+require 'sync'
+
+require 'wordnet/constants'
+require 'wordnet/synset'
 
 module WordNet
 
-	##
-	# Lexicon exception - something has gone wrong in the internals of the
-	# lexicon.
+	### Lexicon exception - something has gone wrong in the internals of the
+	### lexicon.
 	class LexiconError < StandardError ; end
 
-	##
-	# Lookup error - the object being looked up either doesn't exist or is
-	# malformed
+	### Lookup error - the object being looked up either doesn't exist or is
+	### malformed
 	class LookupError < StandardError ; end
 
-	##
-	# Locking error - An attempt was made to write to a lexicon without first
-	# unlocking it.
-	class LockError < StandardError ; end
+	### WordNet lexicon class - abstracts access to the WordNet lexical
+	### databases, and provides factory methods for looking up and creating new
+	### WordNet::Synset objects.
+	class Lexicon
 
-	##
-	# WordNet lexicon class - abstracts access to the WordNet lexical databases,
-	# and provides factory methods for looking up and creating new
-	# WordNet::Synset objects.
-	class Lexicon < Object
-
-		##
 		# Class constants
-		Version = /([\d\.]+)/.match( %q$Revision: 1.2 $ )[1]
-		Rcsid = %q$Id: lexicon.rb,v 1.2 2002/01/14 13:41:27 deveiant Exp $
+		Version = /([\d\.]+)/.match( %q{$Revision: 1.3 $} )[1]
+		Rcsid = %q$Id: lexicon.rb,v 1.3 2003/08/06 08:07:04 deveiant Exp $
 
-		### Public methods
+		#############################################################
+		###	B E R K E L E Y D B   C O N F I G U R A T I O N
+		#############################################################
+
+		# The path to the WordNet BerkeleyDB Env. It lives in the directory that
+		# this module is in.
+		DbFile = File::join( File::dirname(__FILE__), "lexicon" )
+
+		# Options for the creation of the Env object
+		EnvOptions = {
+			:set_timeout	=> 50,
+			:set_lk_detect	=> 1,
+			:set_verbose	=> false,
+		}
+
+		# Flags for the creation of the Env object
+		EnvFlags = BDB::CREATE|BDB::INIT_TRANSACTION|BDB::RECOVER
+
+		# Table names (actually database names in BerkeleyDB)
+		TableNames = {
+			:index => "index",
+			:data => "data",
+			:morph => "morph",
+		}
+
+
+
+		#############################################################
+		###	I N S T A N C E   M E T H O D S
+		#############################################################
+
+		### Create a new WordNet::Lexicon object.
+		def initialize
+			Dir::mkdir( DbFile ) unless File::directory?( DbFile )
+
+			@env = BDB::Env::new( DbFile, EnvFlags, EnvOptions )
+			@indexDb = @env.open_db( BDB::BTREE, "index", nil, BDB::CREATE, 0666 )
+			@dataDb = @env.open_db( BDB::BTREE, "data", nil, BDB::CREATE, 0666 )
+			@morphDb = @env.open_db( BDB::BTREE, "morph", nil, BDB::CREATE, 0666 )
+		end
+
+
+		######
 		public
+		######
 
-		##
-		# Create a new WordNet::Lexicon object which will use the databases
-		# in the directory specified by _dictPath_.
-		def initialize( dictPath=WordNet::DICTDIR )
-			@dictPath	= dictPath
-			@locked		= true
-			@mutex		= Sync.new
+		# The BDB::Env object which contains the wordnet lexicon's databases.
+		attr_reader :env
 
-			@indexDb	= BDB::Btree.open( "#{@dictPath}/lingua_wordnet.index", nil, BDB::CREATE, 0666 ) or
-				raise RuntimeError, "Unable to load #{@dictPath}/lingua_wordnet.index: Unknown error."
-			@dataDb		= BDB::Btree.open( "#{@dictPath}/lingua_wordnet.data", nil, BDB::CREATE, 0666 ) or
-				raise RuntimeError, "Unable to load #{@dictPath}/lingua_wordnet.data: Unknown error."
-			@morphDb	= BDB::Btree.open( "#{@dictPath}/lingua_wordnet.morph", nil, BDB::CREATE, 0666 ) or
-				raise RuntimeError, "Unable to load #{@dictPath}/lingua_wordnet.morph: Unknown error."
+		# The handle to the index table
+		attr_reader :indexDb
+		alias_method :index_db, :indexDb
 
-			@active = true
+		# The handle to the synset data table
+		attr_reader :dataDb
+		alias_method :data_db, :dataDb
+
+		# The handle to the morph table
+		attr_reader :morphDb
+		alias_method :morph_db, :morphDb
+
+
+		### Checkpoint the database. (BerkeleyDB-specific)
+		def checkpoint( bytes=0, minutes=0 )
+			@env.checkpoint
 		end
 
-		##
-		# Close the databases if they're currently opened.
-		def close
-			return false unless @active
-			@mutex.synchronize( Sync::EX ) {
-				@indexDb.close
-				@dataDb.close
-				@morphDb.close
-				@active = false
+
+		### Return a list of archival logfiles that can be removed
+		### safely. (BerkeleyDB-specific).
+		def archlogs
+			return @env.log_archive( BDB::ARCH_ABS )
+		end
+
+
+		### Remove any archival logfiles for the lexicon's database
+		### environment. (BerkeleyDB-specific).
+		def cleanLogs
+			self.archlogs.each {|logfile|
+				File::chmod( 0777, logfile )
+				File::delete( logfile )
 			}
 		end
 
-		##
-		# Returns +true+ if the receiver is active (ie., still has open database
-		# connections)
-		def active?
-			@active
-		end
 
-		##
-		# Lock the lexicon to prohibit changes (default).
-		def lock
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			@mutex.synchronize( Sync::EX ) {
-				@locked = true
-			}
-		end
-
-		##
-		# Unlock the lexicon to allow files to be written when data is
-		# added/edited/deleted. If a block is given, it will be called with the
-		# lexicon unlocked, and then be locked again when the block exits.
-		def unlock
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			if block_given?
-				@mutex.synchronize( Sync::EX ) {
-					@locked = false
-					yield
-					@locked = true
-				}
-			else
-				@mutex.synchronize( Sync::EX ) {
-					@locked = false
-				}
-			end
-		end
-
-		##
-		# Returns true if the lexicon is currently preventing writes.
-		def locked?
-			@locked
-		end
-
-		##
-		# Returns an integer of the familiarity/polysemy count for _word_ as a
-		# _partOfSpeech_. Given a third value _polyCount_, sets the polysemy count for
-		# _word_ as a _partOfSpeech_. In this module, this is a value which must be
-		# updated by the user, and is not automatically modified. This makes it
-		# useful for recording familiarity or frequency counts outside of the
-		# Wordnet lexicons. Note that polysemy can be identified for a given
-		# word by counting the synsets returned by #lookupSynsets.
+		### Returns an integer of the familiarity/polysemy count for +word+ as a
+		### +partOfSpeech+. Given a third value +polyCount+, sets the polysemy
+		### count for +word+ as a +partOfSpeech+. In this module, this is a
+		### value which must be updated by the user, and is not automatically
+		### modified. This makes it useful for recording familiarity or
+		### frequency counts outside of the Wordnet lexicons. Note that polysemy
+		### can be identified for a given word by counting the synsets returned
+		### by #lookupSynsets.
 		def familiarity( word, partOfSpeech, polyCount=nil )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			@mutex.synchronize( Sync::SH ) {
-				@indexDb[ "#{word}%#{partOfSpeech}" ].split(Regexp.escape WordNet::DELIM)[0].to_i
-			}
+			ikey = "#{word}%#{partOfSpeech}"
+			return nil unless @indexDb.key?( ikey )
+			@indexDb[ ikey ].split( WordNet::DelimRe ).first.to_i
 		end
 
 
-		##
-		# Look up sysets (Wordnet::Synset objects) matching _text_ as a
-		# _partOfSpeech_, where _partOfSpeech_ is one of +WordNet::NOUN+,
-		# +WordNet::VERB+, +WordNet::ADJECTIVE+, or +WordNet::ADVERB+. Without
-		# _sense_, #lookupSynsets will return all matches that are a
-		# _partOfSpeech_ as an Array object. If _sense_ is specified, only the
-		# synset object that matches that particular _partOfSpeech_ and _sense_
-		# is returned.
+		### Look up sysets (Wordnet::Synset objects) matching +text+ as a
+		### +partOfSpeech+, where +partOfSpeech+ is one of +WordNet::Noun+,
+		### +WordNet::Verb+, +WordNet::Adjective+, or +WordNet::Adverb+. Without
+		### +sense+, #lookupSynsets will return all matches that are a
+		### +partOfSpeech+. If +sense+ is specified, only the synset object that
+		### matches that particular +partOfSpeech+ and +sense+ is returned.
 		def lookupSynsets( word, partOfSpeech, sense=nil )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			poly, offsets = nil, nil
+			wordkey = "#{word}%#{partOfSpeech}"
+			synsets = []
 
-			test = :test
+			# Look up the index entry, trying first the word as given, and if
+			# that fails, trying morphological conversion.
+			entry = @indexDb[ wordkey ]
+			if entry.nil? && (word = self.morph( word, partOfSpeech ))
+				entry = @indexDb[ wordkey ]
+			end
 
-			# Serialize access to the database
-			@mutex.synchronize( Sync::SH ) {
+			# If the lookup failed both ways, just abort
+			return nil unless entry
 
-				# If the database doesn't have the specified word, try to morph
-				# it and look it up again.
-				if ! @indexDb.has_key?( "#{word}%#{partOfSpeech}" )
-					word = self.morph( word, partOfSpeech )
-					if ! word || ! @indexDb.has_key?( "#{word}%#{partOfSpeech}" )
-						return sense ? nil : []
-					end
-				end
+			# Get the offsets from the entry, narrowing it to just the sense
+			# requested if one was specified.
+			offsets = entry.split( WordNet::DelimRe )[1].
+				split( WordNet::SubDelimRe ).
+				collect {|off| "#{off}%#{partOfSpeech}" }
 
-				poly, offsets = @indexDb[ "#{word}%#{partOfSpeech}" ].split( Regexp.escape WordNet::DELIM )
-			}
-
-			# If we got a sense, just return that particular synset
 			if sense
-				offset = offsets.split( Regexp.escape WordNet::SUBDELIM )[ sense - 1 ] + "%#{partOfSpeech}"
-				return lookupSynsetByOffset( offset )
-
-			# Otherwise, return all of 'em
+				return lookupSynsetsByOffset( offsets[sense - 1] )
 			else
-				synsets = []
-				offsets.split( Regexp.escape WordNet::SUBDELIM ).each {|off|
-					synsets.push lookupSynsetByOffset( "#{off}%#{partOfSpeech}" )
-				}
-				return synsets
+				return [ lookupSynsetsByOffset(*offsets) ].flatten
 			end
 		end
-		alias :lookup_synset :lookupSynsets
-		alias :lookupSynset :lookupSynsets
+		alias_method :lookup_synsets, :lookupSynsets
 
-		##
-		# Returns a WordNet::Synset object with the _offset_ specified.
-		def lookupSynsetByOffset( offset )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			@mutex.synchronize( Sync::SH ) {
-				raise LookupError, "Failed lookup of synset '#{offset}': No such synset" unless
-					@dataDb.has_key?( offset )
+
+		### Returns the WordNet::Synset objects corresponding to the +offsets+
+		### specified.
+		def lookupSynsetsByOffset( *offsets )
+			synsets = []
+
+			offsets.each {|offset|
+				raise LookupError, "Failed lookup of synset '#{offset}':"\
+					"No such synset" unless @dataDb.key?( offset )
 
 				data = @dataDb[ offset ]
 				partOfSpeech = offset[-1,1]
-				return WordNet::Synset::new( self, offset, partOfSpeech, nil, data )
+				synsets << Synset::new( self, offset, partOfSpeech, nil, data )
 			}
-		end
-		alias :lookup_synset_offset :lookupSynsetByOffset
-		alias :lookupSynsetOffset :lookupSynsetByOffset
 
-		##
-		# Returns a form of _word_ as a part of speech _partOfSpeech_, as found in the
-		# WordNet morph files. The #lookupSynsets method perfoms morphological
-		# conversion automatically, so a call to #morph is not required.
+			return *synsets
+		end
+		alias_method :lookup_synsets_by_offset, :lookupSynsetsByOffset
+
+
+		### Returns a form of +word+ as a part of speech +partOfSpeech+, as
+		### found in the WordNet morph files. The #lookupSynsets method perfoms
+		### morphological conversion automatically, so a call to #morph is not
+		### required.
 		def morph( word, partOfSpeech )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			@mutex.synchronize( Sync::SH ) {
-				return @morphDb[ "#{word}%#{partOfSpeech}" ]
-			}
+			return @morphDb[ "#{word}%#{partOfSpeech}" ]
 		end
 
-		##
-		# Returns the result of looking up _word_ in the inverse of the WordNet
-		# morph files. _(This is undocumented in Lingua::Wordnet)_
+
+		### Returns the result of looking up +word+ in the inverse of the WordNet
+		### morph files. _(This is undocumented in Lingua::Wordnet)_
 		def reverseMorph( word )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			@mutex.synchronize( Sync::SH ) {
-				return @morphDb.invert[ word ]
-			}
+			@morphDb.invert[ word ]
 		end
-		alias :reverse_morph :reverseMorph
+		alias_method :reverse_morph, :reverseMorph
 
-		##
-		# Returns an array of compound words matching _text_.
+
+		### Returns an array of compound words matching +text+.
 		def grep( text )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
 			return [] if text.empty?
 			
 			words = []
 			
 			# Grab a cursor into the database and fetch while the key matches
 			# the target text
-			@mutex.synchronize( Sync::SH ) {
-				cursor = @indexDb.cursor
-				rec = cursor.set_range( text )
-				while rec[0] =~ /^#{text}/
-					words.push rec[0]
-					rec = cursor.next
-				end
-			}
+			cursor = @indexDb.cursor
+			rec = cursor.set_range( text )
+			while /^#{text}/ =~ rec[0]
+				words.push rec[0]
+				rec = cursor.next
+			end
+			cursor.close
 
-			return words
+			return *words
 		end
 
-		##
-		# Factory method: Creates and returns a new WordNet::Synset object in
-		# this lexicon for the specified _word_ and _partOfSpeech_.
+
+		### Factory method: Creates and returns a new WordNet::Synset object in
+		### this lexicon for the specified +word+ and +partOfSpeech+.
 		def createSynset( word, partOfSpeech )
-			raise LexiconError, "Can't reuse inactive lexicon object" unless @active
-			return WordNet::Synset::new( self, '', partOfSpeech, word )
+			return Synset::new( self, '', partOfSpeech, word )
 		end
-		alias :new_synset :createSynset
-		alias :newSynset :createSynset
+		alias_method :new_synset, :createSynset
+		alias_method :newSynset, :createSynset
 
-		##
-		# Store the specified WordNet::Synset object in the lexicon. Returns the
-		# offset of the stored synset.
+
+		### Store the specified +synset+ (a WordNet::Synset object) in the
+		### lexicon. Returns the offset of the stored synset.
 		def storeSynset( synset )
-			raise LockError, "Cannot write to a locked lexicon." if @locked
-
 			strippedOffset = nil
 			partOfSpeech = synset.partOfSpeech
 
-			@mutex.synchronize( Sync::EX ) {
-			
+			# Start a transaction
+			@env.begin( BDB::TXN_COMMIT, @dataDb ) do |txn,datadb|
+
 				# If this is a new synset, generate an offset for it
-				if synset.offset =~ /^1%(\w)$/
+				if /^1%(\w)$/ =~ synset.offset
 					partOfSpeech = $1
-					@dataDb['offsetcount'] = @dataDb['offsetcount'].to_i + 1
-					strippedOffset = @dataDb['offsetcount']
+					strippedOffset =
+						(datadb['offsetcount'] = datadb['offsetcount'].to_i + 1)
 					synset.offset = strippedOffset + "%#{partOfSpeech}"
 				else
 					strippedOffset = synset.offset.gsub( /%\w/, '' )
 				end
 				
 				# Write the data entry
-				@dataDb[ synset.offset ] = synset.serialize
+				datadb[ synset.offset ] = synset.serialize
 					
 				# Write the index entries
-				synset.words.collect {|word| word + "%" + partOfSpeech }.each {|word|
+				txn.begin( BDB::TXN_COMMIT, @indexDb ) do |txn,indexdb|
+					synset.words.collect {|word| word + "%" + partOfSpeech }.
+						each {|word|
 
-					# If the index already has this word, but not this synset, add it
-					if @indexDb.has_key?( word )
-						unless @indexDb[ word ] =~ strippedOffset
-							@indexDb[ word ] << WordNet::SUBDELIM << strippedOffset
+						# If the index already has this word, but not this
+						# synset, add it
+						if indexdb.key?( word )
+							unless indexdb[ word ].include?( strippedOffset )
+								indexdb[ word ] << WordNet::SubDelim <<
+									strippedOffset
+							end
+						else
+							indexdb[ word ] = "1" << WordNet::Delim <<
+								strippedOffset
 						end
-					else
-						@indexDb[ word ] = "1" << WordNet::DELIM << strippedOffset
-					end
-				}
-			}
+					}
+				end # transaction on @indexDb
+			end # transaction on @dataDB
 
 			return synset.offset
 		end
-
+		alias_method :store_synset, :storeSynset
 
 	end # class Lexicon
 end # module WordNet
