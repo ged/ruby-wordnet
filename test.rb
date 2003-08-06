c2a7122aed56e70d6b93e4bc31f62675a95311af
Michael Granger <ged@FaerieMUD.org>
2003-08-06 08:01:30 +0000

    - Converted to use Test::Unit.

--- test.rb
+++ test.rb
@@ -1,47 +1,105 @@
 #!/usr/bin/ruby
 #
-#	Test suite for Ruby-WordNet
+#	Test::Unit test suite for Ruby-WordNet
 #
 #
 
-require "runit/testsuite"
-require "runit/cui/testrunner"
-require "parsearg"
-require "find"
+BEGIN {
+	$:.unshift "lib", "tests"
 
-# RUNIT::CUI::TestRunner.quiet_mode = true
+	require './utils'
+	include UtilityFunctions
+}
 
-$: << "lib"
+verboseOff {
+	require 'tests/wntestcase'
+	require 'find'
+	require 'test/unit'
+	require 'test/unit/testsuite'
+	require 'test/unit/ui/console/testrunner'
+	require 'optparse'
+}
 
-### Run 'em, or run the ones that match Regexp.new(ARGV[0]), if specified.
-class TestAll
-	def TestAll.suite
+# Turn off output buffering
+$stderr.sync = $stdout.sync = true
 
-		# Load all the tests from the tests dir
-		puts "Finding test files..."
-		Find.find("t") {|file|
-			Find.prune if file =~ %r{^\.\.|.*/\.}
-			next if File.stat( file ).directory?
-			next unless file =~ /.*\.rb$/
-			puts "  Requiring #{file}..."
-			require( file )
-		}
-		# Find all the loaded test classes
-		testClasses = []
-		ObjectSpace.each_object( Class ) {|klass|
-			next unless klass < RUNIT::TestCase
-			testClasses << klass
-		}
+# Initialize variables
+patterns = []
+requires = []
 
-		# Define a test suite made up of all the RUNIT::TestCase suites
-		suite = RUNIT::TestSuite.new
-		testClasses.sort.each {|klass|
-			suite.add(klass.suite)
-		}
-		return suite
+# Parse command-line switches
+ARGV.options {|oparser|
+	oparser.banner = "Usage: #$0 [options] [TARGETS]\n"
+
+	oparser.on( "--debug", "-d", TrueClass, "Turn debugging on" ) {
+		$DEBUG = true
+		debugMsg "Turned debugging on."
+	}
+
+	oparser.on( "--verbose", "-v", TrueClass, "Make progress verbose" ) {
+		$VERBOSE = true
+		debugMsg "Turned verbose on."
+	}
+
+	# Handle the 'help' option
+	oparser.on( "--help", "-h", "Display this text." ) {
+		$stderr.puts oparser
+		exit!(0)
+	}
+
+	oparser.parse!
+}
+
+# Parse test patterns
+ARGV.each {|pat| patterns << Regexp::new( pat, Regexp::IGNORECASE )}
+$stderr.puts "#{patterns.length} patterns given on the command line"
+
+### Load all the tests from the tests dir
+Find.find("tests") {|file|
+	Find.prune if /\/\./ =~ file or /~$/ =~ file
+	Find.prune if /TEMPLATE/ =~ file
+	next if File.stat( file ).directory?
+
+ 	unless patterns.empty?
+ 		Find.prune unless patterns.find {|pat| pat =~ file}
+ 	end
+
+	debugMsg "Considering '%s': " % file
+	next unless file =~ /\.tests.rb$/
+	debugMsg "Requiring '%s'..." % file
+	require "#{file}"
+	requires << file
+}
+
+$stderr.puts "Required #{requires.length} files."
+unless patterns.empty?
+	$stderr.puts "[" + requires.sort.join( ", " ) + "]"
+end
+
+# Build the test suite
+class WordNetTests
+	class << self
+		def suite
+			suite = Test::Unit::TestSuite.new( "WordNet" )
+
+			if suite.respond_to?( :add )
+				ObjectSpace.each_object( Class ) {|klass|
+					suite.add( klass.suite ) if klass < WordNet::TestCase
+				}
+			else
+				ObjectSpace.each_object( Class ) {|klass|
+					suite << klass.suite if klass < WordNet::TestCase
+				}			
+			end
+
+			return suite
+		end
 	end
 end
 
-RUNIT::CUI::TestRunner.run( TestAll.suite )
+# Run tests
+Test::Unit::UI::Console::TestRunner.new( WordNetTests ).start
+
+
 
 
