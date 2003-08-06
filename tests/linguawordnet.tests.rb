6c61f2e4e434a9729c20991a97f63cee656d8374
Michael Granger <ged@FaerieMUD.org>
2003-08-06 08:08:27 +0000

    - Converted to use Test::Unit.
    --HG--
    extra : convert_revision : svn%3Abc0cae06-b0e3-0310-b5ad-c18dca479551/trunk%4038

--- tests/linguawordnet.tests.rb
+++ tests/linguawordnet.tests.rb
@@ -1,54 +1,36 @@
 #!/usr/bin/ruby
 
-# This is a port of the test.pl script that comes with Lingua::Wordnet. It's
-# here to make sure this library is at least functionally compatible.
+require "wntestcase"
 
-$: << "../lib" if File.directory?( "../lib" )
-$: << "lib" if File.directory?( "lib" )
+class LexiconTests < WordNet::TestCase
 
-require "runit/cui/testrunner"
-require "runit/testcase"
+	$lexicon ||= WordNet::Lexicon::new
 
-require "WordNet"
+	BaseballOffset = '00447188%n'
 
-class LinguaWordnetTests < RUNIT::TestCase
-
-	def setup
-		@lexicon = WordNet::Lexicon.new
-	end
-
-	def teardown
-		@lexicon = nil
-	end
-
-	def test_pl
+	def test_00_PerlTests
 		synset = nil
 		synset2 = nil
 
-		assert_no_exception { synset = @lexicon.lookupSynsetByOffset( "00333350%n" ) }
+		assert_nothing_raised {
+			synset = $lexicon.lookupSynsetsByOffset( BaseballOffset )
+		}
 		assert_instance_of WordNet::Synset, synset
 
 		words = ''
-
 		synset.hyponyms.each {|bb_synset|
 			bb_synset.words += ["ballser"]
 			bb_synset.words.each {|word| words += "#{word}, "}
 		}
-
-		assert_match words, /hardball/
+		assert_match( /hardball/, words )
 
 		words = ''
-		synset2 = @lexicon.lookupSynsets( "travel", WordNet::VERB, 2 )
+		synset2 = $lexicon.lookupSynsets( "travel", WordNet::Verb, 2 )
 		synset2.words.each {|word| words += "#{word}, "}
-		
-		assert_match words, /journey/
-		assert_equal @lexicon.familiarity("boy", WordNet::NOUN), 4
-		assert_equal @lexicon.morph("bluest", WordNet::ADJECTIVE), "blue"
-		assert_match "#{synset}", /baseball/
+		assert_match( /journey/, words )
+
+		assert_equal $lexicon.familiarity( "boy", WordNet::Noun ), 4
+		assert_equal $lexicon.morph( "bluest", WordNet::Adjective ), "blue"
+		assert_match( /baseball/, "#{synset}" )
 	end
 end
-
-if $0 == __FILE__
-    RUNIT::CUI::TestRunner.run(LinguaWordnetTests.suite)
-end
-
