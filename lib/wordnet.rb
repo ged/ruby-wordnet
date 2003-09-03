98a6a8ad12275c6545cf64775939b3e9afc2a81c
Michael Granger <ged@FaerieMUD.org>
2003-09-03 06:27:51 +0000

    - Updated synopsis to reflect changes to the API.
    - Added optional require of CrossCase module.

--- lib/wordnet.rb
+++ lib/wordnet.rb
@@ -5,33 +5,31 @@
 # 
 #   require "WordNet"
 # 
-#   # Create a new lexicon object, and unlock it so we can write to it
-#   wn = WordNet::Lexicon.new( "/usr/share/wordnet" )
-#   wn.unlock
+#   # Create a new lexicon object
+#   lex = WordNet::Lexicon::new
 # 
 #   # Look up the synsets for "language" and "computer program"
-#   languageSynset = wn.lookupSynset( "language", NOUN, 3 )
-#   programSynset = wn.lookupSynset( "program", NOUN, 3 )
+#   languageSynset = lex.lookupSynsets( "language", WordNet::Noun, 3 )
+#   programSynset = lex.lookupSynsets( "program", WordNet::Noun, 3 )
 # 
 #   # Create a new synset for programming languages, set its gloss, link it to its
 #   # hypernym and holonym, and save everything to the database.
-#   progLangSynset = wn.createSynset( "programming language", NOUN )
+#   progLangSynset = lex.createSynset( "programming language", WordNet::Noun )
 #   progLangSynset.gloss = "a system of human-readable symbols and words "\
 #		"for encoding instructions for a computer"
-#   progLangSynset.hypernyms += languageSynset ; languageSynset.hyponyms += progLangSynset
-#   progLangSynset.holonyms += programSynset ; programSynset.meronyms += progLangSynset
-#   for (progLangSynset, programSynset, languageSynset ) do |synset|
-# 	  synset.send( "write" )
+#   progLangSynset.hypernyms += languageSynset
+#   languageSynset.hyponyms += progLangSynset
+#   progLangSynset.holonyms += programSynset
+#	programSynset.stuffMeronyms += progLangSynset
+#   [ progLangSynset, programSynset, languageSynset ].each do |synset|
+# 	  synset.store
 #   end
 # 
 #   # Create a new synset for Ruby, link it, and save it
-#   rubySynset = wn.createSynset( "Ruby", NOUN )
+#   rubySynset = lex.createSynset( "Ruby", Wordnet::Noun )
 #   rubySynset.gloss = "an interpreted scripting language for quick and easy object-oriented programming"
 #   rubySynset.hypernyms += languageSyn ; languageSynset.hyponyms += rubySyn
 #   rubySynset.write ; languageSynset.write
-# 
-#   # Now lock the lexicon to prevent further writes
-#   wn.close
 # 
 # == Description
 #
@@ -59,14 +57,24 @@
 #
 # == Version
 #
-#  $Id: wordnet.rb,v 1.3 2003/08/06 08:04:43 deveiant Exp $
+#  $Id: wordnet.rb,v 1.4 2003/09/03 06:27:51 deveiant Exp $
 #
 
+# Try to provide underbarred alternatives for camelCased methods. Requires the
+# 'CrossCase' module.
+begin
+	require 'crosscase'
+rescue LoadError
+end
+
+### The main namespace for WordNet classes
 module WordNet
 
-	### Class constants
-	Version = /([\d\.]+)/.match( %q{$Revision: 1.3 $} )[1]
-	Rcsid = %q$Id: wordnet.rb,v 1.3 2003/08/06 08:04:43 deveiant Exp $
+	# CVS version tag
+	Version = /([\d\.]+)/.match( %q{$Revision: 1.4 $} )[1]
+
+	# CVS id tag
+	Rcsid = %q$Id: wordnet.rb,v 1.4 2003/09/03 06:27:51 deveiant Exp $
 
 end # module WordNet
 
