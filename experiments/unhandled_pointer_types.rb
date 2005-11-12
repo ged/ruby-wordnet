4028d5c2454a67590bba98fc4c35230ddaad13ac
Michael Granger <ged@FaerieMUD.org>
2005-11-12 18:14:54 +0000

    * Adding minimal testcase for missing synset pointer types bug. (refs #3)

---
+++ experiments/unhandled_pointer_types.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+#
+# Trying to find a minimal testcase for reproducing the error that happens with
+# the new '~i' and '@i' synset pointer types.
+#
+# Time-stamp: <12-Nov-2005 13:11:04 ged>
+#
+
+BEGIN {
+	base = File::dirname( File::dirname(File::expand_path(__FILE__)) )
+	$LOAD_PATH.unshift "#{base}/lib"
+
+	require "#{base}/utils.rb"
+	include UtilityFunctions
+}
+
+require 'wordnet'
+
+lex = WordNet::Lexicon.new
+syn = lex.lookup_synsets_by_offset( "04456674%n" )
+
+try( %{syn.pointerlist} )
+try( %{syn.pointers} )
+
