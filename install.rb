096f48ae66df31167c6c6f67b625b37b33bfd9e2
Michael Granger <ged@FaerieMUD.org>
2005-11-12 12:21:45 +0000

    * Propagated updates from project-utils.
    --HG--
    extra : convert_revision : svn%3Abc0cae06-b0e3-0310-b5ad-c18dca479551/trunk%4062

--- install.rb
+++ install.rb
@@ -1,93 +1,185 @@
 #!/usr/bin/ruby
 #
-# $Date: 2003/09/03 05:32:47 $
-# Copyright (c) 2000, 2003 Masatoshi SEKI
+#	Module Install Script
+#	$Id$
 #
-# install.rb is copyrighted free software by Masatoshi SEKI.
-# You can redistribute it and/or modify it under the same term as Ruby.
+#	Thanks to Masatoshi SEKI for ideas found in his install.rb.
+#
+#	Copyright (c) 2001-2005 The FaerieMUD Consortium.
+#
+#	This is free software. You may use, modify, and/or redistribute this
+#	software under the terms of the Perl Artistic License. (See
+#	http://language.perl.com/misc/Artistic.html)
+#
 
-$LOAD_PATH.unshift "."
+require './utils.rb'
+include UtilityFunctions
 
 require 'rbconfig'
+include Config
+
 require 'find'
 require 'ftools'
-require 'utils'
+require 'optparse'
 
-include Config, UtilityFunctions
+$version	= %q$Revision: 11 $
+$rcsId		= %q$Id$
 
-CheckDb = 'lib/wordnet/lexicon'
+# Define required libraries
+RequiredLibraries = [
+	# libraryname, nice name, RAA URL, Download URL, e.g.,
+	[ 'bdb', "Berkeley-DB", 
+		'http://raa.ruby-lang.org/project/bdb',
+		'',
+	],
+]
 
 class Installer
-	protected
-	def install(from, to, mode = nil, verbose = false)
-		str = "install '#{from}' to '#{to}'"
-		str += ", mode=#{mode}" if mode
-		puts str if verbose
+
+	@@PrunePatterns = [
+		/CVS/,
+		/~$/,
+		%r:(^|/)\.:,
+		/\.tpl$/,
+	]
+
+	def initialize( testing=false )
+		@ftools = (testing) ? self : File
 	end
 
-	protected
-	def makedirs(*dirs)
-		for d in dirs
-			puts "mkdir #{d}"
+	### Make the specified dirs (which can be a String or an Array of Strings)
+	### with the specified mode.
+	def makedirs( dirs, mode=0755, verbose=false )
+		dirs = [ dirs ] unless dirs.is_a? Array
+
+		oldumask = File::umask
+		File::umask( 0777 - mode )
+
+		for dir in dirs
+			if @ftools == File
+				File::mkpath( dir, $verbose )
+			else
+				$stderr.puts "Make path %s with mode %o" % [ dir, mode ]
+			end
 		end
+
+		File::umask( oldumask )
 	end
 
-	def initialize(test=false)
-		@version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
-		@libdir = File.join(CONFIG["libdir"], "ruby", @version)
-		@sitelib = find_site_libdir
-		@ftools = (test) ? self : File
-	end
-	public
-	attr_reader(:libdir, :sitelib)
-
-	private
-	def find_site_libdir
-		site_libdir = $:.find {|x| x =~ /site_ruby$/}
-		if !site_libdir
-			site_libdir = File.join(@libdir, "site_ruby")
-		elsif site_libdir !~ Regexp::new( Regexp.quote(@version) )
-			site_libdir = File.join(site_libdir, @version)
-		end
-		site_libdir
-	end
-
-	public
-	def files_in_dir(dir)
-		list = []
-		Find.find(dir) do |f|
-			list.push(f)
-		end
-		list
-	end
-
-	public
-	def install_files(srcdir, files, destdir=@sitelib)
-		path = []
-		dir = []
-
-		for f in files
-			next if (f = f[srcdir.length+1..-1]) == nil
-			path.push f if File.ftype(File.join(srcdir, f)) == 'file'
-			dir |= [ File.dirname(File.join(destdir, f)) ]
-		end
-		@ftools.makedirs(*dir)
-		for f in path
-			@ftools.install(File.join(srcdir, f), File.join(destdir, f), nil, true)
+	def install( srcfile, dstfile, mode=nil, verbose=false )
+		dstfile = File.catname(srcfile, dstfile)
+		unless FileTest.exist? dstfile and File.cmp srcfile, dstfile
+			$stderr.puts "   install #{srcfile} -> #{dstfile}"
+		else
+			$stderr.puts "   skipping #{dstfile}: unchanged"
 		end
 	end
 
 	public
-	def install_rb
-		flist = files_in_dir('lib').reject {|fname|
-			/CVS/ =~ fname or
-			/~$/ =~ fname or
-			/^#/ =~ fname
+
+	def installFiles( src, dstDir, mode=0444, verbose=false )
+		directories = []
+		files = []
+		
+		if File.directory?( src )
+			Find.find( src ) {|f|
+				Find.prune if @@PrunePatterns.find {|pat| f =~ pat}
+				next if f == src
+
+				if FileTest.directory?( f )
+					directories << f.gsub( /^#{src}#{File::Separator}/, '' )
+					next 
+
+				elsif FileTest.file?( f )
+					files << f.gsub( /^#{src}#{File::Separator}/, '' )
+
+				else
+					Find.prune
+				end
+			}
+		else
+			files << File.basename( src )
+			src = File.dirname( src )
+		end
+		
+		dirs = [ dstDir ]
+		dirs |= directories.collect {|d| File.join(dstDir,d)}
+		makedirs( dirs, 0755, verbose )
+		files.each {|f|
+			srcfile = File.join(src,f)
+			dstfile = File.dirname(File.join( dstDir,f ))
+
+			if verbose
+				if mode
+					$stderr.puts "Install #{srcfile} -> #{dstfile} (mode %o)" % mode
+				else
+					$stderr.puts "Install #{srcfile} -> #{dstfile}"
+				end
+			end
+
+			@ftools.install( srcfile, dstfile, mode, verbose )
 		}
-		install_files('lib', flist)
 	end
+
 end
 
-inst = Installer.new( ARGV.shift == '-n' )
-inst.install_rb
+
+if $0 == __FILE__
+	dryrun = false
+
+	# Parse command-line switches
+	ARGV.options {|oparser|
+		oparser.banner = "Usage: #$0 [options]\n"
+
+		oparser.on( "--verbose", "-v", TrueClass, "Make progress verbose" ) {
+			$VERBOSE = true
+			debugMsg "Turned verbose on."
+		}
+
+		oparser.on( "--dry-run", "-n", TrueClass, "Don't really install anything" ) {
+			debugMsg "Turned dry-run on."
+			dryrun = true
+		}
+
+		# Handle the 'help' option
+		oparser.on( "--help", "-h", "Display this text." ) {
+			$stderr.puts oparser
+			exit!(0)
+		}
+
+		oparser.parse!
+	}
+
+	# Don't do anything if they expect this to be the three-step install script
+	# and they aren't doing the 'install' step.
+	if ARGV.include?( "config" )
+		for lib in RequiredLibraries
+			testForRequiredLibrary( *lib )
+		end
+		puts "Done."
+	elsif ARGV.include?( "setup" )
+		puts "Done."
+	elsif ARGV.empty?
+		for lib in RequiredLibraries
+			testForRequiredLibrary( *lib )
+		end
+	end
+
+	if ARGV.empty? || ARGV.include?( "install" )
+		debugMsg "Sitelibdir = '#{CONFIG['sitelibdir']}'"
+		sitelibdir = CONFIG['sitelibdir']
+		debugMsg "Sitearchdir = '#{CONFIG['sitearchdir']}'"
+		sitearchdir = CONFIG['sitearchdir']
+
+		message "Installing..."
+		i = Installer.new( dryrun )
+		#i.installFiles( "redist", sitelibdir, 0444, verbose )
+		i.installFiles( "lib", sitelibdir, 0444, $VERBOSE )
+
+		message "done.\n"
+	end
+end
+	
+
+
 
