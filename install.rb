456fb0fa352317546a8eafd558bed5f033f4364a
Michael Granger <ged@FaerieMUD.org>
2005-12-08 11:01:42 +0000

    - Synced utility scripts with project-utils.
    --HG--
    extra : convert_revision : svn%3Abc0cae06-b0e3-0310-b5ad-c18dca479551/trunk%4065

--- install.rb
+++ install.rb
@@ -12,11 +12,19 @@
 #	http://language.perl.com/misc/Artistic.html)
 #
 
-require './utils.rb'
-include UtilityFunctions
+BEGIN {
+    basedir = File::dirname( __FILE__ )
+    $LOAD_PATH.unshift( "#{basedir}/lib" )
+    require 'wordnet'
+
+    require "#{basedir}/utils.rb"
+    include UtilityFunctions
+}
+
+require './convertdb.rb'
 
 require 'rbconfig'
-include Config
+include Config unless defined?( CONFIG )
 
 require 'find'
 require 'ftools'
@@ -33,6 +41,60 @@ RequiredLibraries = [
 		'',
 	],
 ]
+
+DataDir = File::join( File::expand_path(File::dirname(__FILE__)), 
+    File::basename(WordNet::Lexicon::DefaultDbEnv) )
+
+def main( dryrun, forcedbcreate, *args )
+
+	# Don't do anything if they expect this to be the three-step install script
+	# and they aren't doing the 'install' step.
+	if args.include?( "config" )
+		for lib in RequiredLibraries
+			testForRequiredLibrary( *lib )
+		end
+		puts "Done."
+	elsif args.include?( "setup" )
+        makeDatabase()
+		puts "Done."
+	elsif args.empty?
+		for lib in RequiredLibraries
+			testForRequiredLibrary( *lib )
+		end
+	end
+
+	if args.empty? || args.include?( "install" )
+	    makeDatabase()
+		sitelibdir = CONFIG['sitelibdir']
+		debugMsg "Sitelibdir = '#{sitelibdir}'"
+		sitearchdir = CONFIG['sitearchdir']
+		debugMsg "Sitearchdir = '#{sitearchdir}'"
+		datadir = WordNet::Lexicon::DefaultDbEnv
+		debugMsg "Datadir = '#{datadir}'"
+
+		message "Installing..."
+		message "\n" if $VERBOSE
+
+		i = Installer.new( dryrun )
+		i.installFiles( DataDir, datadir, 0666 - File::umask, $VERBOSE )
+		i.installFiles( "lib", sitelibdir, 0444, $VERBOSE )
+
+		message "done.\n"
+	end
+end
+
+
+### If the database hasn't already been built, or +force+ is true, build
+### it out of the WordNet data files.
+def makeDatabase( force=false )
+    return unless force ||
+        !File.directory?( DataDir ) || 
+        !File.exists?( DataDir + "/index" )
+    
+    debugMsg( "(Re)-creating the WordNet database" )
+    convertdb()
+end
+
 
 class Installer
 
@@ -57,7 +119,7 @@ class Installer
 
 		for dir in dirs
 			if @ftools == File
-				File::mkpath( dir, $verbose )
+				File::mkpath( dir, $VERBOSE )
 			else
 				$stderr.puts "Make path %s with mode %o" % [ dir, mode ]
 			end
@@ -82,43 +144,60 @@ class Installer
 		files = []
 		
 		if File.directory?( src )
-			Find.find( src ) {|f|
-				Find.prune if @@PrunePatterns.find {|pat| f =~ pat}
-				next if f == src
+			Find.find( src ) do |f|
+			    verboseMsg "Examining '#{f}'"
+				if pattern = @@PrunePatterns.find {|pat| f =~ pat}
+				    debugMsg "Pruning #{f}: Matched pattern #{pattern.inspect}" 
+				    Find.prune
+			    end
+
+				if f == src
+				    debugMsg "Nexting in Find loop on origin directory"
+				    next
+			    else
+			        debugMsg "No next, because %p != %p" %
+			            [ f, src ]
+			    end
 
 				if FileTest.directory?( f )
+				    verboseMsg "Adding directory '#{f}'"
 					directories << f.gsub( /^#{src}#{File::Separator}/, '' )
 					next 
 
 				elsif FileTest.file?( f )
+				    verboseMsg "Adding file '#{f}'"
 					files << f.gsub( /^#{src}#{File::Separator}/, '' )
 
 				else
+				    verboseMsg "Pruning '#{f}' (not a file or directory)"
 					Find.prune
 				end
-			}
+			end
 		else
+            verboseMsg "Directly adding file '#{f}'"
 			files << File.basename( src )
 			src = File.dirname( src )
 		end
-		
+
+		verboseMsg "Done with file search for '#{src}'"
+
 		dirs = [ dstDir ]
 		dirs |= directories.collect {|d| File.join(dstDir,d)}
 		makedirs( dirs, 0755, verbose )
-		files.each {|f|
+		files.each do |f|
 			srcfile = File.join(src,f)
 			dstfile = File.dirname(File.join( dstDir,f ))
 
 			if verbose
 				if mode
-					$stderr.puts "Install #{srcfile} -> #{dstfile} (mode %o)" % mode
+					verboseMsg "Install #{srcfile} -> #{dstfile} (mode %o)" % mode
 				else
-					$stderr.puts "Install #{srcfile} -> #{dstfile}"
+					verboseMsg "Install #{srcfile} -> #{dstfile}"
 				end
 			end
 
 			@ftools.install( srcfile, dstfile, mode, verbose )
-		}
+		end
 	end
 
 end
@@ -126,58 +205,42 @@ end
 
 if $0 == __FILE__
 	dryrun = false
+	forcedbcreate = false
 
 	# Parse command-line switches
 	ARGV.options {|oparser|
 		oparser.banner = "Usage: #$0 [options]\n"
 
-		oparser.on( "--verbose", "-v", TrueClass, "Make progress verbose" ) {
+		oparser.on( "--debug", "-d", TrueClass, "Show debugging output" ) do
+		    $DEBUG = true
+		    debugMsg "Turned debugging on."
+	    end
+
+		oparser.on( "--verbose", "-v", TrueClass, "Make progress verbose" ) do
 			$VERBOSE = true
 			debugMsg "Turned verbose on."
-		}
-
-		oparser.on( "--dry-run", "-n", TrueClass, "Don't really install anything" ) {
+		end
+		
+		oparser.on( "--dry-run", "-n", TrueClass, "Don't really install anything" ) do
 			debugMsg "Turned dry-run on."
 			dryrun = true
-		}
+		end
+
+        oparser.on( "--force-db-create", "-f", FalseClass, "Force creation of the database" ) do
+            debugMsg "Will force re-creation of the Ruby-WordNet database"
+            forcedbcreate = true
+        end
 
 		# Handle the 'help' option
-		oparser.on( "--help", "-h", "Display this text." ) {
+		oparser.on( "--help", "-h", "Display this text." ) do
 			$stderr.puts oparser
 			exit!(0)
-		}
+		end
 
 		oparser.parse!
 	}
 
-	# Don't do anything if they expect this to be the three-step install script
-	# and they aren't doing the 'install' step.
-	if ARGV.include?( "config" )
-		for lib in RequiredLibraries
-			testForRequiredLibrary( *lib )
-		end
-		puts "Done."
-	elsif ARGV.include?( "setup" )
-		puts "Done."
-	elsif ARGV.empty?
-		for lib in RequiredLibraries
-			testForRequiredLibrary( *lib )
-		end
-	end
-
-	if ARGV.empty? || ARGV.include?( "install" )
-		debugMsg "Sitelibdir = '#{CONFIG['sitelibdir']}'"
-		sitelibdir = CONFIG['sitelibdir']
-		debugMsg "Sitearchdir = '#{CONFIG['sitearchdir']}'"
-		sitearchdir = CONFIG['sitearchdir']
-
-		message "Installing..."
-		i = Installer.new( dryrun )
-		#i.installFiles( "redist", sitelibdir, 0444, verbose )
-		i.installFiles( "lib", sitelibdir, 0444, $VERBOSE )
-
-		message "done.\n"
-	end
+    main( dryrun, forcedbcreate, *ARGV )
 end
 	
 
