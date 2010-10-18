16bbf0b456532531c05ff8da6a459e84f7510025
Michael Granger <ged@FaerieMUD.org>
2010-10-18 16:54:46 -0700

    Starting update for Ruby 1.9.2/RSpec 2.0
    --HG--
    branch : sequel-conversion

--- spec/wordnet/lexicon_spec.rb
+++ spec/wordnet/lexicon_spec.rb
@@ -9,21 +9,10 @@ BEGIN {
 	$LOAD_PATH.unshift( libdir ) unless $LOAD_PATH.include?( libdir )
 }
 
-begin
-	require 'fileutils'
-	require 'tmpdir'
-	require 'bdb'
-	require 'spec/runner'
-	require 'spec/lib/helpers'
+require 'spec'
+require 'spec/lib/helpers'
 
-	require 'wordnet/lexicon'
-rescue LoadError
-	unless Object.const_defined?( :Gem )
-		require 'rubygems'
-		retry
-	end
-	raise
-end
+require 'wordnet/lexicon'
 
 
 
@@ -56,24 +45,22 @@ describe WordNet::Lexicon do
 		FileUtils.rm_rf @path, :verbose => $DEBUG
 	end
 
-	
+
 
 	#################################################################
 	###	T E S T S
 	#################################################################
 
 	it "defaults to being in :readonly mode" do
-		env = stub( "bdb environment handle", :open_db => nil )
-		BDB::Env.should_receive( :new ).
-			with( @path.to_s, WordNet::Lexicon::ENV_FLAGS_RO, WordNet::Lexicon::ENV_OPTIONS ).
-			and_return( env )
+		db = mock( "database handle" )
+		Sequel.should_receive( :connect )
 
 		lex = WordNet::Lexicon.new( @path.to_s )
 
 		lex.should be_readonly()
 		lex.should_not be_readwrite()
 	end
-	
+
 	it "can be created in :writable mode" do
 		env = stub( "bdb environment handle", :open_db => nil )
 		BDB::Env.should_receive( :new ).
@@ -85,7 +72,7 @@ describe WordNet::Lexicon do
 		lex.should_not be_readonly()
 		lex.should be_readwrite()
 	end
-	
+
 	it "passes a read/write flagset to BDB when created in :readwrite mode" do
 		env = stub( "bdb environment handle", :open_db => nil )
 		BDB::Env.should_receive( :new ).
@@ -97,7 +84,7 @@ describe WordNet::Lexicon do
 		lex.should_not be_readonly()
 		lex.should be_readwrite()
 	end
-	
+
 
 	describe "created in readonly mode" do
 
@@ -114,8 +101,8 @@ describe WordNet::Lexicon do
 			@env.should_not_receive( :log_archive )
 			@lexicon.clean_logs
 		end
-		
-		
+
+
 	end
 
 
@@ -128,7 +115,7 @@ describe WordNet::Lexicon do
 
 			@lexicon = WordNet::Lexicon.new( @path.to_s, :readwrite )
 		end
-		
+
 
 		it "can be closed" do
 			@env.should_receive( :close )
@@ -139,7 +126,7 @@ describe WordNet::Lexicon do
 			@env.should_receive( :checkpoint )
 			@lexicon.checkpoint
 		end
-	
+
 		it "provides an interface to clean up database transaction logs" do
 			@env.should_receive( :log_archive ).with( BDB::ARCH_ABS ).
 				and_return([ :log1, :log2 ])
@@ -147,13 +134,13 @@ describe WordNet::Lexicon do
 			File.should_receive( :delete ).with( :log1 )
 			File.should_receive( :chmod ).with( 0777, :log2 )
 			File.should_receive( :delete ).with( :log2 )
-			
+
 			@lexicon.clean_logs
 		end
-		
-	
+
+
 	end
-	
+
 
 	describe "with a converted WordNet database" do
 
@@ -175,8 +162,8 @@ describe WordNet::Lexicon do
 				@lexicon.familiarity( word, pos ).should be_an_instance_of( Fixnum )
 			end
 		end
-		
-		
+
+
 		it "returns the root word as the morphological conversion of a dictionary word it knows about" do
 			@lexicon.morph( "angriest", WordNet::Adjective ).should == 'angry'
 		end
@@ -194,7 +181,7 @@ describe WordNet::Lexicon do
 
 		it "tries looking up a failing via its morphological conversion if the original fails" do
 			synsets = @lexicon.lookup_synsets( 'angriest', WordNet::Adjective )
-			
+
 			synsets.should_not be_nil()
 			synsets.first.should be_an_instance_of( WordNet::Synset )
 			synsets.first.words.should include( 'angry' )
@@ -206,8 +193,8 @@ describe WordNet::Lexicon do
 			synset.should be_an_instance_of( WordNet::Synset )
 			synset.words.first.should =~ /operate/i
 		end
-		
-		
+
+
 		it "can find every compound sense of a word in its dictionary" do
 			words = @lexicon.grep( 'thing' )
 
@@ -223,8 +210,8 @@ describe WordNet::Lexicon do
 			words.should include( 'thingumajig%n' )
 			words.should include( 'thingummy%n' )
 		end
-		
-		
+
+
 		TEST_WORDS.each do |word, pos|
 			it "can look up the synset #{word}(#{pos}) by word and part-of-speech" do
 				synsets = @lexicon.lookup_synsets( word, pos )
