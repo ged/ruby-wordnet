44339599bf0df13bdd2166e7896897ea621eff21
Michael Granger <ged@FaerieMUD.org>
2011-07-14 16:21:55 -0700

    Adding a build dir for the default DB gem
    --HG--
    branch : sequel-conversion

---
+++ wordnet-defaultdb/lib/wordnet-defaultdb.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+# This gem is a container for the default WordNetSQL database files required for
+# the 'wordnet' gem. It's mostly just a wrapper around the Sqlite database from:
+#
+#   http://sourceforge.net/projects/wnsql/
+# 
+# @author Michael Granger <ged@FaerieMUD.org>
+# 
+module WordNet
+	module DefaultDB
+
+		# Library version constant
+		VERSION = '0.99.0'
+
+		# Version-control revision constant
+		REVISION = %q$Revision$
+
+	end # module DefaultDB
+end # module Wordnet
+
