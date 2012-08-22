3db1391f27a9785aa8bfe04084ac635e206439e2
Michael Granger <ged@FaerieMUD.org>
2012-08-22 11:52:03 -0700

    Add tests for some uncovered Synset features.

--- spec/wordnet/synset_spec.rb
+++ spec/wordnet/synset_spec.rb
@@ -55,31 +55,29 @@ describe WordNet::Synset, :requires_database => true do
 
 	context "for 'floor, level, storey, story (noun)' [103365991]" do
 
-		before( :each ) do
-			# floor, level, storey, story (noun): [noun.artifact] a structure
-			#     consisting of a room or set of rooms at a single position along a
-			#     vertical scale (hypernym: 1, hyponym: 5, part meronym: 1)
-			@synset = WordNet::Synset[ 103365991 ]
-		end
+		# floor, level, storey, story (noun): [noun.artifact] a structure
+		#     consisting of a room or set of rooms at a single position along a
+		#     vertical scale (hypernym: 1, hyponym: 5, part meronym: 1)
+		let( :synset ) { WordNet::Synset[103365991] }
 
 
 		it "knows what lexical domain it's from" do
-			@synset.lexical_domain.should == 'noun.artifact'
+			synset.lexical_domain.should == 'noun.artifact'
 		end
 
 		it "can make a Sequel dataset for any of its semantic link relationships" do
-			ds = @synset.semanticlink_dataset( :member_meronyms )
+			ds = synset.semanticlink_dataset( :member_meronyms )
 			ds.should be_a( Sequel::Dataset )
 			ds.first_source_table.should == :synsets
 		end
 
 		it "can make an Enumerator for any of its semantic link relationships" do
-			enum = @synset.semanticlink_enum( :member_meronyms )
+			enum = synset.semanticlink_enum( :member_meronyms )
 			enum.should be_a( Enumerator )
 		end
 
 		it "can recursively traverse its semantic links" do
-			res = @synset.traverse( :hypernyms ).to_a
+			res = synset.traverse( :hypernyms ).to_a
 			res.should have( 6 ).members
 			res.should == [
 				WordNet::Synset[ 104341686 ],
@@ -92,7 +90,7 @@ describe WordNet::Synset, :requires_database => true do
 		end
 
 		it "can return an Enumerator for recursively traversing its semantic links" do
-			enum = @synset.traverse( :hypernyms )
+			enum = synset.traverse( :hypernyms )
 
 			enum.next.should == WordNet::Synset[ 104341686 ]
 			enum.next.should == WordNet::Synset[ 100021939 ]
@@ -104,23 +102,44 @@ describe WordNet::Synset, :requires_database => true do
 				enum.next
 			}.to raise_error( StopIteration )
 		end
+	end
 
-
-		end
 
 	context "for 'knight (noun)' [110238375]" do
 
-		before( :each ) do
-			@synset = @lexicon[ :knight, "noble" ]
-		end
+		let( :synset ) { @lexicon[:knight, "noble"] }
 
 		it "can find the hypernym that it and another synset share in common through the intersection operator" do
-			res = @synset | @lexicon[ :squire ]
+			res = synset | @lexicon[ :squire ]
 			res.should == @lexicon[:person]
+		end
+
+		it "knows what part of speech it's for" do
+			synset.part_of_speech.should == 'noun'
+		end
+
+		it "stringifies as a readable definition" do
+			synset.to_s.should == "knight (noun): [noun.person] originally a person of noble birth trained to " +
+				"arms and chivalry; today in Great Britain a person honored by the sovereign for personal " +
+				"merit (hypernym: 1, hyponym: 6, instance hyponym: 1)"
 		end
 
 	end
 
+
+	context "for 'congener (noun)' [100003993]" do
+
+		let( :synset ) { @lexicon[:congener] }
+
+		it "can look up its sample sentences" do
+			synset.samples.should have( 2 ).members
+			synset.samples.should include(
+				"lard was also used, though its congener, butter, was more frequently employed",
+				"the American shopkeeper differs from his European congener"
+			)
+		end
+
+	end
 
 
 end
